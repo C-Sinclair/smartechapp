c7c62307ae53c7b5fbd57bb2c118c09b6ef84938
Manjaro <conor@wilxite.com>
2019-12-02 00:45:10 +0000

    most of the dial tests in

--- App/components/Dial/Dial.spec.tsx
+++ App/components/Dial/Dial.spec.tsx
@@ -1,25 +1,32 @@
-// import * as React from 'react';
-// import { render } from 'react-native-testing-library'
-// import Page from '../Page/Page'
+import * as React from 'react';
+import { render, fireEvent } from 'react-native-testing-library'
+import Dial from '../Dial/Dial'
+import SpriteSheet from 'rn-sprite-sheet'
 
 describe('Dial', () => {
-    // const { queryByTestId, getByTestId } = render(<Page selectedIcon={1} />);
 
     describe('rendering', () => {
 
         it('should display correct image for temperature', () => {
-
+            const { getByType } = render(<Dial temp={24} />);
+            expect(getByType(SpriteSheet).props.source).toEqual(require('../../../assets/sprite.png'))
         })
 
         it('should display temperature figure', () => {
-
+            const { getByTestId } = render(<Dial temp={24} />);
+            expect(getByTestId("tempFigure").props.children).toEqual("24°")
         })
     });
 
     describe('events', () => {
 
         it('should change temperature on +/- button press', () => {
+            const { getByTestId } = render(<Dial temp={24} />);
+            fireEvent.press(getByTestId("plus"))
+            expect(getByTestId("tempFigure").props.children).toEqual("25°")
 
+            fireEvent.press(getByTestId("minus"))
+            expect(getByTestId("tempFigure").props.children).toEqual("24°")
         })
 
         it('should adjust temperature on touch of dial', () => {
@@ -27,7 +34,18 @@ describe('Dial', () => {
         })
 
         it('should not be able to go below 0 or above 360', () => {
-
+            const minus = () => {
+                const { getByTestId } = render(<Dial temp={0} />);
+                fireEvent.press(getByTestId("minus"))
+                expect(getByTestId("tempFigure").props.children).toEqual("0°")
+            }
+            const plus = () => {
+                const { getByTestId } = render(<Dial temp={360} />);
+                fireEvent.press(getByTestId("plus"))
+                expect(getByTestId("tempFigure").props.children).toEqual("360°")
+            }
+            minus()
+            plus()
         })
 
         it('should produce clicking sound on temperature change', () => {
